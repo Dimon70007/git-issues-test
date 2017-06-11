1951dc29ad369a20424b9af93d5eed8f8a70b202
Dimon70007 <dimon.fly.mail@mail.ru>
2017-06-11 14:25:18 +0300

    configureStore has been refactored

---
+++ src/store/configureStore.prod.js
@@ -0,0 +1,16 @@
+
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducers from '../reducers';
+import promisesMiddleware from '../middleware/promises';
+
+
+const logger = createLogger();
+const configureStore = preloadedState => createStore(
+  reducers,
+  preloadedState,
+  applyMiddleware(thunk, logger, promisesMiddleware),
+);
+
+export default configureStore;
